0682750463484faf7ba12177b5bbdae0df5af2a0
Michael Petersen <mpetason@gmail.com>
2016-06-08 13:46:38 -0700

    Reworked functions for faster results.

--- launchpad_report.py
+++ launchpad_report.py
@@ -8,27 +8,14 @@ import argparse
 cachedir = ".launchpadlib/cache/"
 launchpad = Launchpad.login_anonymously('just testing', 'production', cachedir, version='devel')
 
-def created_bugs(users):
+def created_bugs(user, start_date):
     output = {}
-    for user in users:
-        for i in launchpad.people(user).searchTasks(status=[
-                                "New","Opinion","Invalid",
-                                "Won't Fix","Expired","Confirmed","Triaged",
-                                "In Progress","Fix Committed","Fix Released",
-                                "Incomplete (with response)",
-                                "Incomplete (without response)"]):
-             if i.owner.name == user:
-                 output.setdefault(user, [])
-                 output[user].append(i)
-    return output
- 
-def filter_by_days(bugs, num_days, start_date):
-    date_range = datetime.today() - timedelta(days=num_days)
-    output = []
-    for bug in bugs:
-        if bug.date_created.replace(tzinfo=None) > date_range:
-            output.append(bug)
-    return output
+    lp_user = launchpad.people(user)
+    return lp_user.searchTasks(owner=lp_user,created_since=start_date,status=[
+                   "New","Opinion","Invalid","Won't Fix","Expired","Confirmed","Triaged",
+                   "In Progress","Fix Committed","Fix Released","Incomplete (with response)",
+                   "Incomplete (without response)"
+                   ])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Launchpad bug Reports.')
@@ -42,12 +29,15 @@ if __name__ == "__main__":
     if not args.usernames:
         pass
     else:
-        bug_reporters = args.usernames
-        bugs = created_bugs(bug_reporters)
+        if not args.date:
+            start_date = datetime.today() - timedelta(days=args.days)
+        else: 
+            start_date = args.date
         filtered_bugs = {}
-        for user in bug_reporters:
+        for user in args.usernames:
+            filtered_bugs[user] = created_bugs(user, start_date)
             bug_count = 0
-            filtered_bugs[user] = filter_by_days(bugs[user], args.days, args.date)
-            for i in filtered_bugs[user]:
+            for bug in filtered_bugs[user]:
                 bug_count = bug_count + 1
-                print "[" + str(bug_count) + "]" + "[" + user + "]" +  "[" + i.web_link + "]"
+                print "[" + str(bug_count) + "]" + "[" + user + "]" + "[" + bug.web_link + "]"
+
